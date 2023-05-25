eda37cf9c8399cf2b4ff9335a47c0f9d17732c87
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-05-25 15:53:38 -0500

    Actualizar home.jsx

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,27 @@
-import React from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState, useEffect } from 'react';
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+function Home() {
+  const [contador, setContador] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setContador((prevContador) => (prevContador + 1) % 1000000);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const contadorString = contador.toString().padStart(6, '0');
+
+  return (
+    <div>
+      {contadorString.split('').map((digito, index) => (
+        <Card key={index} props ={digito} />
+      ))}
+    </div>
+  );
+}
 };
 
 export default Home;
