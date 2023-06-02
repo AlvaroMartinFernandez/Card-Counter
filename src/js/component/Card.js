8056a008e19232e1c757b5eed7b2efd06871db10
Alvaro Martin Fernandez <91843474+AlvaroMartinFernandez@users.noreply.github.com>
2023-06-02 18:20:45 +0000

    Finalizado

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,7 +1,7 @@
 import React from 'react'
 export default function Card({props}){
     return (
-        <div className="card col-1">
+        <div className="card col-1 bg-secondary text-white ">
             <div className="card-body">
                 <h1 className="card-text">{props}</h1>
             </div>
